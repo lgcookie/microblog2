e0ed756f61569b3355297a209cfe03f3ce637a9b
lgcookie <louisgreen1998@hotmail.co.uk>
2021-01-03 15:57:10 +0000

    chapter17

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, 'env.env'))
 
 
 class Config(object):
@@ -12,5 +15,9 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['louisgreen1998@hotmail.co.uk']
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 25
+    ELASTICSEARCH_URL= os.environ.get('ELASTICSEARCH_URL')
+    
